c9ea87773e01c007bb4311a3f3201394efb65a6b
Paul Molodowitch <paul@molodowitch.com>
2020-04-15 16:02:21 -0700

    initial commit

---
+++ sepDeclAndDef.cpp
@@ -0,0 +1,10 @@
+// sepDeclAndDef.cpp
+
+#include "sepDeclAndDef.h"
+
+/// Implementation docs
+long SepDeclAndDef::myFunc(
+    int intDefName, char charDeclDefName, long longDefName)
+{
+    return intDefName + longDefName;
+}
---
+++ sepDeclAndDef.h
@@ -0,0 +1,6 @@
+// sepDeclAndDef.h
+
+class SepDeclAndDef {
+public:
+    long myFunc(int intDeclName, char charDeclDefName, long);
+};
